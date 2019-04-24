9fb97fd5fe0348612c6eb5823a0631352326aa5b
zither <325831871@qq.com>
2019-04-24 08:57:25 +0800

    chore(*): learn grammar about sequence

---
+++ hogwarts-basics/datatype/Sequence.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Test sequence feature
+l_foo = ["i", "learn", "python", "now", "and", "you"]
+print(" ".join(l_foo))
+
+print(l_foo[1:3])
+print(l_foo[-3:-1])  # reverse slice
+print(l_foo[:4:2])  # step size
+
+
