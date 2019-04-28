dd6c43c5655d1d48d06b0b459812eef6be167c47
zither <325831871@qq.com>
2019-04-28 09:50:49 +0800

    chore(*): object

---
+++ hogwarts-basics/src/object/Person.py
@@ -0,0 +1,13 @@
+class Person():
+
+    def __init__(self):
+        self.sex = ""
+
+    def age(self):
+        pass
+
+    def set_sex(self, sex):
+        self.sex = sex
+
+    if __name__ == '__main__':
+        pass
